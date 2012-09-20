632014fb93ec8734b85beb28579c844ea927a53a
Ed Shadi <edshadi@gmail.com>
2012-09-20 17:40:24 -0400

    finder methods should be class methods.

--- lib/acts_as_reviewable.rb
+++ lib/acts_as_reviewable.rb
@@ -1,16 +1,28 @@
 require 'active_record'
 module ActsAsReviewable
+
   module Review
-    def find_review_by reviewer
-      where(:reviewer => reviewer).order("created_at DESC")
+    def self.included(receiver)
+      receiver.extend ClassMethods
     end
 
-    def find_review_for reviewable
-      where(["reviewable_type = ? and reviewable_id = ?", reviewable.class.name, reviewable.id]).order("created_at DESC")
+    module ClassMethods
+      def find_reviews_by reviewer
+        where(:reviewer_id => reviewer.id).order("created_at DESC")
+      end
+
+      def find_reviews_for reviewable
+        where(["reviewable_type = ? and reviewable_id = ?", reviewable.class.name, reviewable.id]).order("created_at DESC")
+      end
     end
+
   end
 
   module Reviewable
+    def self.included(receiver)
+      receiver.extend ClassMethods
+    end
+
     module ClassMethods
       def acts_as_reviewable
         has_many :reviews, {:as => :reviewable, :dependent => :destroy}
